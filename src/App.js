09bbe4fa8681f0810021de1c2913d4f44af6f452
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-12-18 15:45:03 +0900

    useState fook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const reset = () => setCount(0)
+  const times = () => setCount(count * 2)
+  // const perThree = () => setCount(count%3!==0?count:count/3)
+  const perThree = () => setCount(previousCount => {
+    if (previousCount%3===0) {
+      return previousCount/3
+    }
+    return previousCount
+  })
+
   return (
-    <div>
-      This is a template for React App.
-    </div>
+    <>
+      <div>
+        <span>count: {count}</span>
+      </div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={times}>×2</button>
+      </div>
+      <div>
+        <button onClick={reset}>reset</button>
+      </div>
+      <div>
+        <button onClick={perThree}>3の倍数のときだけ3で割る</button>
+      </div>
+    </>
   )
 }
 
