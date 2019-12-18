3f91463727a3d1fbae34a3b1c14afc2a229c7480
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-12-18 21:54:03 +0900

    create reducer for events

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,41 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = props => {
+const App = () => {
   return(
     <div className="container-fluid">
-      <section className="my-5">
-        <h1>イベント作成フォーム</h1>
-        <form>
-          <div className="form-group">
-            <label htmlFor="formEventTitle">タイトル</label>
-            <input id="formEventTitle" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="formEventBody">ボディ</label>
-            <textarea id="formEventBody" className="form-control"/>
-          </div>
-        </form>
-        <button className="btn btn-primary">イベントを作成する</button>
-        <button className="btn btn-danger">イベントを削除する</button>
-      </section>
-      <section className="my-5">
-        <h1>イベント一覧</h1>
-        <table className="table table-hover">
-          <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>ボディ</th>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </table>
-      </section>
+      <div className="row">
+        <section className="col-12 col-xl-5 my-5">
+          <h1>イベント作成フォーム</h1>
+          <form>
+            <div className="form-group">
+              <label htmlFor="formEventTitle">タイトル</label>
+              <input id="formEventTitle" className="form-control" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="formEventBody">ボディ</label>
+              <textarea id="formEventBody" className="form-control"/>
+            </div>
+          </form>
+          <button className="btn btn-primary">イベントを作成する</button>
+          <button className="btn btn-danger">イベントを削除する</button>
+        </section>
+        <section className="col-12 col-xl-7 my-5">
+          <h1>イベント一覧</h1>
+          <table className="table table-hover">
+            <tr>
+              <th>ID</th>
+              <th>タイトル</th>
+              <th>ボディ</th>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </table>
+        </section>
+      </div>
     </div>
   )
 }
