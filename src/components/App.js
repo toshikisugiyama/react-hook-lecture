5651d6a1212768c72990b8319e9b7f98397877e9
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-12-18 23:18:38 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState,useReducer } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import reducer from '../reducers'
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const addEvent = e => {
+    e.preventDefault()
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body,
+    })
+    setTitle('')
+    setBody('')
+  }
   return(
     <div className="container-fluid">
       <div className="row">
@@ -10,29 +24,33 @@ const App = () => {
           <form>
             <div className="form-group">
               <label htmlFor="formEventTitle">タイトル</label>
-              <input id="formEventTitle" className="form-control" />
+              <input id="formEventTitle" className="form-control" value={title} onChange={e => setTitle(e.target.value)} />
             </div>
             <div className="form-group">
               <label htmlFor="formEventBody">ボディ</label>
-              <textarea id="formEventBody" className="form-control"/>
+              <textarea id="formEventBody" className="form-control" value={body} onChange={e => setBody(e.target.value)} />
             </div>
           </form>
-          <button className="btn btn-primary">イベントを作成する</button>
+          <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
           <button className="btn btn-danger">イベントを削除する</button>
         </section>
         <section className="col-12 col-xl-7 my-5">
           <h1>イベント一覧</h1>
           <table className="table table-hover">
-            <tr>
-              <th>ID</th>
-              <th>タイトル</th>
-              <th>ボディ</th>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>タイトル</th>
+                <th>ボディ</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
           </table>
         </section>
       </div>
