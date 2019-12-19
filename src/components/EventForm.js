d9883a26eb53a36fc2ec46ed8788de95c17eb45b
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-12-19 16:10:08 +0900

    replace type names with constant

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {CREATE_EVENT, DELETE_ALL_EVENTS} from '../actions'
 
 const EventForm = ({state, dispatch}) => {
   const [title, setTitle] = useState('')
@@ -6,7 +7,7 @@ const EventForm = ({state, dispatch}) => {
   const addEvent = e => {
     e.preventDefault()
     dispatch({
-      type: 'CREATE_EVENT',
+      type: CREATE_EVENT,
       title,
       body,
     })
@@ -18,7 +19,7 @@ const EventForm = ({state, dispatch}) => {
     e.preventDefault()
     const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
     if (result) {
-      dispatch({ type: 'DELETE_ALL_EVENTS' })
+      dispatch({ type: DELETE_ALL_EVENTS })
     }
   }
 
